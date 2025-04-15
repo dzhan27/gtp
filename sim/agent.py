c865024bade294fcc57c46db8a4ace18269b81bd
Daniel Zhan <dzhan27@gmail.com>
2025-04-15 18:04:53 -0400

    type when used alone is a python keyword - switched to gender

--- sim/agent.py
+++ sim/agent.py
@@ -1,10 +1,10 @@
 class Agent:
-    def __init__(self, strategy, position, type=None):
+    def __init__(self, strategy, position, gender=None):
         self.strategy = strategy
         self.score = 0
         self.history = []
         self.position = position
-        self.type = type
+        self.type = gender
         self.prev_score = 0
 
     def reset_score(self):
